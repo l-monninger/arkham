47011d01a1f2eeb32e14609b9940e552edf3ef8d
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-03-26 03:04:33 -0700

    safety

--- services/blocks/blocks/data/transactions_stream/std_transactions_stream/usd_conversion.py
+++ services/blocks/blocks/data/transactions_stream/std_transactions_stream/usd_conversion.py
@@ -2,13 +2,23 @@ from blocks.util.lru.lru import lru_cache_time
 import requests
 import json
 from .wei import wei_to_eth
+from time import sleep
 
 COIN_GECKO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+LAST = 1000
 
-@lru_cache_time(seconds=10, maxsize=1)
+@lru_cache_time(seconds=20, maxsize=1)
 def get_usd_conversion()->float:
-    d = json.loads(requests.get(COIN_GECKO_URL).text)
-    return d["ethereum"]["usd"]
+    global LAST
+    while True:
+        try:
+            d : dict = json.loads(requests.get(COIN_GECKO_URL).text)
+            LAST = d.get("ethereum", {"usd" : LAST})["usd"]
+            return LAST
+        except Exception as e:
+            return LAST
+        
+    
 
 def convert_wei(wei : str)->float:
     return wei_to_eth(wei=wei) * get_usd_conversion()
